259f31ec38f1968d107cc0f9d62bfec0dbd72033
KamillaTadjibaeva <117359460+KamillaTadjibaeva@users.noreply.github.com>
2023-04-15 11:42:46 +0200

    Fixing Testing

--- Testing.py
+++ Testing.py
@@ -5,13 +5,13 @@ import main
 class TestStringMethods(unittest.TestCase):
 
     def test_null(self):
-        self.assertEqual(main.addition(''), 1)
+        self.assertEqual(main.addition(''), 0)
 
     def test_one_number(self):
         self.assertEqual(main.addition('14'), 14)
 
     def test_two_numbers(self):
-        self.assertEqual(main.addition('8, 3'), 11 + 1)
+        self.assertEqual(main.addition('8, 3'), 11)
 
 
 
