fbe5785a22d5c96791e9199a53c7673ffc6cafdc
KamillaTadjibaeva <117359460+KamillaTadjibaeva@users.noreply.github.com>
2023-04-15 11:26:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import string
 
-
 def addition(input: string):
     if input == '':
         return 0
     if len(input.split(',')) == 2:
-        return int(input.split(',', 1)) + int(input.split(',', 2))
+        arr = input.split(',')
+        return int(arr[0]) + int(arr[1])
     if len(input.split(',')) == 1:
         return int(input)
